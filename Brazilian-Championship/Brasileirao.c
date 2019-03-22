00a75577f78b18c301aa2ef7271773d34716dc23
SirZexTru <gustavoausechi@hotmail.com>
2019-03-21 21:17:29 -0300

    Add new necessary functions
    adversariesLogic() and roundResultsCalc().

--- Brazilian-Championship/Brasileirao.c
+++ Brazilian-Championship/Brasileirao.c
@@ -15,49 +15,71 @@
 // - Gols recebidos (GC)
 // - Saldo de Gols (SG)
 
-#include <stdio.h>
-#include <conio.h>
-
 // 1: Construir tabela de resultados e preencher nomes dos times
 // 2: Fazer primeira rodada manual
 // 3: Fazer primeira rodada automatizada
 // 4: Fazer 38 rodadas automatizadas
 
-// TODO: Implement with struct, considering each column an array of 20 spaces
-struct tableData
+#include <stdio.h>
+#include <conio.h>
+
+void insertTeamNamesOnTable(void);
+void adversariesLogic(void);
+void roundResultsCalc(void);
+
+struct tableColumns
 {
-    char name[20];
+    char teamName[20];
     int position[20], points[20], games[20], wins[20], draw[20], loses[20], goalsDone[20], goalsReceived[20], goalsBalance[20];
 };
-struct tableData finalResultTable;
-
-char insertValuesOnTable()
-{
-    finalResultTable.name[0] = "São paulo";
-    finalResultTable.name[1] = "Atlético";
-    finalResultTable.name[2] = "Atlético";
-    finalResultTable.name[3] = "Avaí";
-    finalResultTable.name[4] = "Bahia";
-    finalResultTable.name[5] = "Botafogo";
-    finalResultTable.name[6] = "Ceará";
-    finalResultTable.name[7] = "Chapecoense";
-    finalResultTable.name[8] = "Corinthians";
-    finalResultTable.name[9] = "Cruzeiro";
-    finalResultTable.name[10] = "CSA";
-    finalResultTable.name[11] = "Flamengo";
-    finalResultTable.name[12] = "Fluminense";
-    finalResultTable.name[13] = "Fortaleza";
-    finalResultTable.name[14] = "Goiás ";
-    finalResultTable.name[15] = "Grêmio";
-    finalResultTable.name[16] = "Internacional" ;
-    finalResultTable.name[17] = "Palmeiras ";
-    finalResultTable.name[18] = "Santos ";
-    finalResultTable.name[19] = "São Paulo ";
-    finalResultTable.name[20] = "Vasco";
-}
+struct tableColumns finalResultTable;
 
 void main()
 {
-    insertValuesOnTable();
+    insertTeamNamesOnTable();
+    for (int i=0; i<20; i++)
+    {
+        // TODO: Implement archive ".csv" to show the the results
+        printf("%s\n", finalResultTable.teamName[i]);
+        printf("----------------");
+    }
     getch();
+}
+
+void insertTeamNamesOnTable(void)
+{
+    finalResultTable.teamName[0] = "Atlético - MG";
+    finalResultTable.teamName[1] = "Atlético - PR";
+    finalResultTable.teamName[2] = "Avaí";
+    finalResultTable.teamName[3] = "Bahia";
+    finalResultTable.teamName[4] = "Botafogo";
+    finalResultTable.teamName[5] = "Ceará";
+    finalResultTable.teamName[6] = "Chapecoense";
+    finalResultTable.teamName[7] = "Corinthians";
+    finalResultTable.teamName[8] = "Cruzeiro";
+    finalResultTable.teamName[9] = "CSA";
+    finalResultTable.teamName[10] = "Flamengo";
+    finalResultTable.teamName[11] = "Fluminense";
+    finalResultTable.teamName[12] = "Fortaleza";
+    finalResultTable.teamName[13] = "Goiás";
+    finalResultTable.teamName[14] = "Grêmio";
+    finalResultTable.teamName[15] = "Internacional" ;
+    finalResultTable.teamName[16] = "Palmeiras";
+    finalResultTable.teamName[17] = "Santos";
+    finalResultTable.teamName[18] = "São Paulo";
+    finalResultTable.teamName[19] = "Vasco";
+}
+
+void adversariesLogic(void)
+{
+    finalResultTable.teamName[0];
+}
+
+void roundResultsCalc(void)
+{
+    for (int i=0; i<=38; i++)
+    {
+        // TODO: Calculate all results and save the values
+        finalResultTable.games[0];
+    }
 }
